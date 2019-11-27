e12163781d3d56f5dc5ac044f9ad61cca3c45e37
Jia Bao <zhangjiabao306@pingan.com.cn>
2019-11-26 22:32:20 -0800

    第一次提交

---
+++ web/src/main/java/com/config/Application.java
@@ -0,0 +1,14 @@
+package com.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhangjiabao on 29/05/2018.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
